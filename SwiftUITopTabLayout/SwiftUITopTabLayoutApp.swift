ced7ebceb0b2df29682b33d98b41f13e997bf515
manuaravind <manuaravindpta@gmail.com>
2024-10-11 21:45:25 +0530

    Initial Commit

---
+++ SwiftUITopTabLayout/SwiftUITopTabLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITopTabLayoutApp.swift
+//  SwiftUITopTabLayout
+//
+//  Created by Manu Aravind on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITopTabLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
